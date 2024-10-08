12c9d0acd3d614e44110643cfe8c98d8f5de266e
Demonbyflame <Demonbyflame@gmail.com>
2024-10-08 21:57:43 +0300

    Add files via upload

---
+++ Task1.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Task1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите число a: ");
+        int a = scanner.nextInt();
+
+        System.out.print("Введите число b: ");
+        int b = scanner.nextInt();
+
+        // Сравнение
+        if (a > b) {
+            System.out.println("a > b");
+        } else if (a < b) {
+            System.out.println("a < b");
+        } else {
+            System.out.println("a = b");
+        }
+
+        // Операции
+        System.out.println("a + b = " + (a + b));
+        System.out.println("a - b = " + (a - b));
+        System.out.println("a * b = " + (a * b));
+        if (b != 0) {
+            System.out.println("a / b = " + (a / (double) b)); // Деление с плавающей точкой
+        } else {
+            System.out.println("Деление на ноль невозможно");
+        }
+    }
+}
